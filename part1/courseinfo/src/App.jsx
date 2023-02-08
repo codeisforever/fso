912c685030b1b7348f229b686f543bfbeb7316ae
codeisforever <123796947+codeisforever@users.noreply.github.com>
2023-02-08 10:08:26 -0500

    completed exercises 1.1 and 1.2 - courseinfo

--- part1/courseinfo/src/App.jsx
+++ part1/courseinfo/src/App.jsx
@@ -1,27 +1,40 @@
-const App = () => {
-  const course = 'Half Stack application development'
-  const part1 = 'Fundamentals of React'
-  const exercises1 = 10
-  const part2 = 'Using props to pass data'
-  const exercises2 = 7
-  const part3 = 'State of a component'
-  const exercises3 = 14
+const part1 = "Fundamentals of React";
+const exercises1 = 10;
+const part2 = "Using props to pass data";
+const exercises2 = 7;
+const part3 = "State of a component";
+const exercises3 = 14;
 
+const Header = () => {
+  return <h1>Half Stack application development</h1>;
+};
+const Part = (prop) => {
+  return (
+    <p>
+      {prop.name} {prop.exerciseCount}
+    </p>
+  );
+};
+const Content = () => {
   return (
     <div>
-      <h1>{course}</h1>
-      <p>
-        {part1} {exercises1}
-      </p>
-      <p>
-        {part2} {exercises2}
-      </p>
-      <p>
-        {part3} {exercises3}
-      </p>
-      <p>Number of exercises {exercises1 + exercises2 + exercises3}</p>
+      <Part name={part1} exerciseCount={exercises1} />
+      <Part name={part2} exerciseCount={exercises2} />
+      <Part name={part3} exerciseCount={exercises3} />
     </div>
-  )
-}
+  );
+};
+const Total = () => {
+  return <p>Number of exercises {exercises1 + exercises2 + exercises3}</p>;
+};
+const App = () => {
+  return (
+    <div>
+      <Header />
+      <Content />
+      <Total />
+    </div>
+  );
+};
 
-export default App
+export default App;
